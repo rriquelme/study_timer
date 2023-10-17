f9b26f729df7d2f79a8ead059cc19f500f87cb40
Roberto Riquelme <rers.92@gmail.com>
2023-10-18 00:04:34 +0800

    refresh ui issue

--- charts.py
+++ charts.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 import json
+import calendar
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QGridLayout, QPushButton, QLineEdit
 from PyQt5.QtGui import QColor, QPainter, QBrush
 from PyQt5.QtCore import Qt, QRect, QPoint, QSize, QDate
@@ -16,23 +17,12 @@ class HabitTracker(QWidget):
 
     def initUI(self):
         # Create a header row with the days of the month for October
-        habit_row = QHBoxLayout()
-        day_label = QLabel("Habits:")
-        habit_row.addWidget(day_label)
-        for i in range(1, 32):
-            day_label = QLabel(str(i))
-            #day_label = QLabel(str(i) + "(" + QDate(2023, 10, i).toString("ddd")[0] + ")")
-            habit_row.addWidget(day_label)
-
-        # Create a grid layout for the habits
-        self.grid = QGridLayout()
-        self.grid.addLayout(habit_row, 0, 0)
-        self.habit_rows = []
-        self.load_habits()
-        for habit in self.habits:
-            self.add_habit_row(habit["name"], habit["squares"])
-
+        self.hrows = QHBoxLayout()
+        #self.setLayout(self.hrows)
+        # First will be the header for the habits
         # Create a new row for the button and habit input
+        self.refresh_gui()
+        self.refresh_gui()
         button_row = QHBoxLayout()
         self.habit_input = QLineEdit()
         button_row.addWidget(self.habit_input)
@@ -40,60 +30,72 @@ class HabitTracker(QWidget):
         self.new_habit_button.clicked.connect(self.add_new_habit)
         button_row.addWidget(self.new_habit_button)
 
+
         # Add the grid and button to the main layout
         main_layout = QVBoxLayout()
-        main_layout.addLayout(self.grid)
+        main_layout.addLayout(self.hrows)
         main_layout.addLayout(button_row)
-
         self.setLayout(main_layout)
 
-    def add_habit_row(self, habit_name, squares):
-        habit_row = QHBoxLayout()
-        day_label = QLabel(habit_name)
-        habit_row.addWidget(day_label)
 
-        # For each habit row, create a colored square for each day of the month
-        habit_squares = []
-        for i in range(1, 32):
-            day = QDate(2023, 10, i)
-            #print(day.toString("ddd")[0])
-            square = Square(day)
-            if squares[i-1] == "green":
-                square.color = Qt.green
+    def refresh_gui(self):
+        self.load_habits()
+        now = datetime.datetime.now()
+        len_month = calendar.monthrange(now.year, now.month)[1]
+        v0 = QVBoxLayout()
+        day_label = QLabel("Habits:")
+        v0.addWidget(day_label)
+        for habit in self.habits:
+            day_label = QLabel(habit['name'])
+            v0.addWidget(day_label)
+        self.hrows.addLayout(v0)
+        
+        # Then the days of the month
+        for i in range(1, len_month+1):
+            vx = QVBoxLayout()
+            day_label = QLabel(str(i))
+            vx.addWidget(day_label)
+            for habit in self.habits:
+                vx.addWidget(self.to_square(habit['days'],i))
+            self.hrows.addLayout(vx)
+
+
+
+            #day_label = QLabel(str(i))
+            #hrows.addWidget(day_label)
+
+
+        
+    def to_square(self, color,ndate):
+        day = QDate(2023, 10, ndate)
+        square = Square(day)
+        if color == "green":
+            square.color = Qt.green
+        else:
+            if datetime.datetime.today().day == day.day():
+                square.color = Qt.yellow
+            elif day.toString("ddd")[0] == 'd' or day.toString("ddd")[0] == 's':
+                square.color = Qt.gray
             else:
-                if datetime.datetime.today().day == day.day():
-                    square.color = Qt.yellow
-                elif day.toString("ddd")[0] == 'd' or day.toString("ddd")[0] == 's':
-                    square.color = Qt.gray
-                else:
-                    square.color = Qt.white
-            habit_squares.append(square)
-            habit_row.addWidget(square)
-
-        self.grid.addLayout(habit_row, len(self.habit_rows)+1, 0)
-        self.habit_rows.append((habit_name, habit_squares))
+                square.color = Qt.white
+        return square
 
     def add_new_habit(self):
         habit_name = self.habit_input.text()
         if habit_name:
-            row = len(self.habit_rows) + 1
-            squares = ["white"] * 31
-            self.add_habit_row(habit_name, squares)
+            d = {}
+            now = datetime.datetime.now()
+            len_month = calendar.monthrange(now.year, now.month)[1]
+            for x in range(1,len_month+1):
+                d[x] = "white"
+            self.habits.append({"name": habit_name, "days": d})
             self.habit_input.clear()
             self.save_habits()
+        self.refresh_gui()
 
     def save_habits(self):
-        habits = []
-        for habit_name, habit_squares in self.habit_rows:
-            squares = []
-            for square in habit_squares:
-                if square.color == Qt.green:
-                    squares.append("green")
-                else:
-                    squares.append("white")
-            habits.append({"name": habit_name, "squares": squares})
         with open("habits.json", "w") as f:
-            json.dump(habits, f, indent=4)
+            json.dump(self.habits, f, indent=4)
 
     def load_habits(self):
         try:
